53bd2e877293e580e4473d4e2e0c8b47f4252a8e
Gabriel Jesus <gabriel.developer2@gmail.com>
2024-10-10 17:25:10 -0300

    chore: config setup test e2e

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,9 +1,8 @@
-import { ConflictException, UsePipes } from "@nestjs/common";
-import { Body, Controller, HttpCode, Post } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
-import { hash } from 'bcryptjs'
-import {z} from 'zod'
+import { Body, ConflictException, Controller, HttpCode, Post, UsePipes } from "@nestjs/common";
+import { hash } from 'bcryptjs';
 import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { PrismaService } from "src/prisma/prisma.service";
+import { z } from 'zod';
 
 const createAccountBodySchema = z.object({
   name: z.string(),
--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,13 +1,14 @@
-import { ConflictException, UseGuards, UsePipes } from "@nestjs/common";
-import { Body, Controller, HttpCode, Post } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
-import { hash } from 'bcryptjs'
-import {z} from 'zod'
-import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { Body, Controller, HttpCode, Post, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { CurrentUser } from "src/auth/current-user-decorator";
+import { UserPayload } from "src/auth/jwt-strategy";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { PrismaService } from "src/prisma/prisma.service";
+import { z } from "zod";
 
 const createQuestionBodySchema = z.object({
-  name: z.string(),
+  title: z.string(),
+  content: z.string(),
 })
 
 type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
@@ -18,34 +19,35 @@ type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 export class CreateQuestionController {
   constructor(
     private prisma: PrismaService
-  ){}
+  ) { }
 
   @Post()
   @HttpCode(201)
-  @UsePipes( new ZodValidationPipe(createQuestionBodySchema))
-  async handle(@Body() body: CreateQuestionBodySchema) {
-    const {name} = body
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Body(new ZodValidationPipe(createQuestionBodySchema)) body: CreateQuestionBodySchema
+  ) {
+    const { title, content } = body
+    const userId = user.sub
+    const slug = this.convertToSlug(title)
 
+    await this.prisma.question.create({
+      data: {
+        userId,
+        title,
+        content,
+        slug: slug
+      }
+    })
+  }
 
-    // const userWithSameEmail = await this.prisma.user.findUnique({
-    //   where: {
-    //     email
-    //   }
-    // })
-
-    // if(userWithSameEmail) {
-    //   throw new ConflictException('User with same e-email address already exist')
-    // }
-
-    // const hashedPassword = await hash(password, 8)
-
-    // await this.prisma.user.create({
-    //   data: {
-    //     name,
-    //     email,
-    //     password: hashedPassword
-    //   }
-    // })
-    return name
+  
+  private convertToSlug(title: string): string {
+    return title
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/[^\w\s-]/g, '')
+      .replace(/\s+/g, '-')
   }
 }
